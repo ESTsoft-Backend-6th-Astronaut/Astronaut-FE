8b31c0fd13257f0537f7ea776d99fde743fcebc8
HBumzz <choi12920@gmail.com>
2024-11-29 10:30:46 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+const Home = () => <h1>Home Page</h1>;
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">About Us</h1>
+      <p className="text-lg text-gray-700 max-w-md text-center">
+        Welcome to the About page! Here, we share information about our mission,
+        vision, and the team behind this project.re glad to have you here!
+      </p>
+    </div>
+  );
+}
+
+export default About;
