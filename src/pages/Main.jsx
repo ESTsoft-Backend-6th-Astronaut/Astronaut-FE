04522afb2ca725d70f9da0bb050533bd65a3999e
cjyjiyoon <cjiyoon1130@gmail.com>
2024-12-18 11:37:35 +0900

    STYLE: 메인 페이지 패딩 설정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,42 +11,43 @@ function Main() {
     <>
       {/* 네비게이션 바 */}
       <NavBar />
+      <div style={{ padding: '0 200px 50px 200px' }}>
+        {/* 상단 섹션: KeywordChart와 KeywordRank */}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center', // 가운데 정렬
+            alignItems: 'flex-start',
+            gap: 4, // 간격 설정
+            mt: 4, // 위 여백 추가
+          }}
+        >
+          <Box sx={{ flex: 1, maxWidth: '45%' }}>
+            <KeywordChart />
+          </Box>
+          <Box sx={{ flex: 1, maxWidth: '45%' }}>
+            <KeywordRank />
+          </Box>
+        </Box>
 
-      {/* 상단 섹션: KeywordChart와 KeywordRank */}
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center', // 가운데 정렬
-          alignItems: 'flex-start',
-          gap: 4, // 간격 설정
-          mt: 4, // 위 여백 추가
-        }}
-      >
-        <Box sx={{ flex: 1, maxWidth: '45%' }}>
-          <KeywordChart />
+        {/* 하단 섹션: PortfolioChart와 PortfolioList */}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center', // 가운데 정렬
+            alignItems: 'flex-start',
+            gap: 4, // 간격 설정
+            mt: 20, // 위 여백 추가
+          }}
+        >
+          <Box sx={{ flex: 1, maxWidth: '45%' }}>
+            <PortfolioChart />
+          </Box>
+          <Box sx={{ flex: 1, maxWidth: '45%' }}>
+            <PortfolioList />
+          </Box>
         </Box>
-        <Box sx={{ flex: 1, maxWidth: '45%' }}>
-          <KeywordRank />
-        </Box>
-      </Box>
-
-      {/* 하단 섹션: PortfolioChart와 PortfolioList */}
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center', // 가운데 정렬
-          alignItems: 'flex-start',
-          gap: 4, // 간격 설정
-          mt: 20, // 위 여백 추가
-        }}
-      >
-        <Box sx={{ flex: 1, maxWidth: '45%' }}>
-          <PortfolioChart />
-        </Box>
-        <Box sx={{ flex: 1, maxWidth: '45%' }}>
-          <PortfolioList />
-        </Box>
-      </Box>
+      </div>
     </>
   );
 }
