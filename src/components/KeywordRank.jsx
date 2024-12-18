ede9f918efa5d19e198bebad75306d43ffb0a89a
cjyjiyoon <cjiyoon1130@gmail.com>
2024-12-18 14:28:10 +0900

    STYLE: url 변경으로 인한 링크 수정, hover시 배경색 바뀌도록 수정

--- src/components/KeywordRank.jsx
+++ src/components/KeywordRank.jsx
@@ -13,11 +13,13 @@ import {
   Paper,
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom'; // React Router로 페이지 이동
+import './KeywordRank.css';
 
 const KeywordRank = () => {
   const [keywords, setKeywords] = useState([]);
   const [error, setError] = useState(null);
   const navigate = useNavigate(); // React Router로 페이지 이동
+  const [hoveringRow, setHoveringRow] = useState(null);
 
   useEffect(() => {
     axios
@@ -33,7 +35,7 @@ const KeywordRank = () => {
   // 클릭 이벤트 핸들러: keywordId를 경로로 사용하여 페이지 이동
   const handleRowClick = (keywordId) => {
     if (keywordId) {
-      navigate(`/${keywordId}/keyword-details`); // 이동할 경로
+      navigate(`/keyword/${keywordId}`); // 이동할 경로
     }
   };
 
@@ -44,8 +46,25 @@ const KeywordRank = () => {
   // interest의 최대값을 찾기
   const maxInterest = Math.max(...keywords.map((keyword) => keyword.interest));
 
+  // 마우스 호버링시 이벤트(색이 어두워짐)
+  const handleMouseOver = (index) => {
+    setHoveringRow(index);
+  };
+
+  const handleMouseOut = () => {
+    setHoveringRow(null);
+  };
+
   return (
-    <Card sx={{ maxWidth: 600, margin: '0 auto', mt: 5 }}>
+    <Card
+      sx={{
+        maxWidth: 600,
+        margin: '0 auto',
+        mt: 5,
+        border: 'none',
+        boxShadow: 'none',
+      }}
+    >
       <CardContent>
         <Typography variant="h5" component="div" gutterBottom>
           인기 키워드 랭킹
@@ -61,7 +80,12 @@ const KeywordRank = () => {
             </TableHead>
             <TableBody>
               {keywords.map((keyword, index) => (
-                <TableRow key={index}>
+                <TableRow
+                  key={index}
+                  className={hoveringRow === index ? 'bg-color' : ''}
+                  onMouseOver={() => handleMouseOver(index)}
+                  onMouseOut={handleMouseOut}
+                >
                   <TableCell>{keyword.ranking}</TableCell>
                   <TableCell
                     onClick={() => handleRowClick(keyword.keywordId)}
